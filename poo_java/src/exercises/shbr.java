e5b50f47c89d1318cb3db22153061049b86b4f1c
thiagoreis96 <thiago.reis96@icloud.com>
2021-02-20 21:37:49 -0300

    ...

---
+++ poo_java/src/exercises/shbr.java
@@ -0,0 +1,55 @@
+package exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class shbr {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Employee> list = new ArrayList<>();
+		
+		System.out.print("How many employess will be registered? ");
+		int N = sc.nextInt();
+		
+		for (int i=0; i<N; i++) {
+			
+			System.out.println();
+			System.out.println("Employee #" + (i + 1) + ":");
+			System.out.print("Id: ");
+			Integer id = sc.nextInt();
+			System.out.println("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.println("Salary: ");
+			Double salary = sc.nextDouble();
+			
+			Employee emp = new Employee(id, name, salary);
+			
+			list.add(emp);
+		}
+		
+		System.out.println("Enter the employee id that will have salary increase: ");
+		int idSalary = sc.nextInt();
+		Integer pos = position(list, idSalary);
+		
+		
+		sc.close();
+	}
+	
+	public static Integer position(List<Employee> list, int id) {
+		for (int i=0; i<list.size(); i++) {
+			if (list.get(i).getId() == id) {
+				return i;
+			}
+		}
+		return null;
+	}
+}
